555b86684ff4277e83439f6a109ab95d14ee1a89
Chris James <quii@hey.com>
2022-07-13 16:00:05 +0100

    todos

--- goodhandlers.go
+++ goodhandlers.go
@@ -12,7 +12,6 @@ type Encoder[T any] func(T, io.Writer) error
 // this is what every "service" layer should look like, defined as types
 type Service[A, B any] func(context.Context, A) (B, error)
 
-// still returns http.handler, so can use with chi, gorilla or just stdlib
 func New[A, B any](service Service[A, B], decode Decoder[A], encode Encoder[B]) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer r.Body.Close()
@@ -21,10 +20,15 @@ func New[A, B any](service Service[A, B], decode Decoder[A], encode Encoder[B]) 
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
+
+		//todo: error handling might be difficult to do nicely, doing 500 as a default is whack
+		//todo: what about response headers?
+
 		response, err := service(r.Context(), body)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 		encode(response, w)
+		w.WriteHeader(http.StatusOK) // holy assumption batman
 	})
 }
